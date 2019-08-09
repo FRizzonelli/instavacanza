eba3bfc6c8ea1668bb2b487883846faf1392d9b5
frizzonelli <fabrizio.rizzonelli@gmail.com>
2019-08-09 15:05:12 +0200

    inital commit

---
+++ styles/colors.ts
@@ -0,0 +1,46 @@
+export enum Colors {
+    /* White */
+
+    WHITE = '#FFFFFF',
+    WHITE_TEXT = '#FFFFFFCC',
+    WHITE_OPACITY_BACKGROUND = '#FAFAFAA3',
+    OVERLAY_OPACITY = '#00000066',
+
+    /* Gray */
+
+    BLACK = '#333333',
+    TOTAL_BLACK = '#000000',
+    GRAY_100 = '#fafafa',
+    GRAY_200 = '#efeff4',
+    GRAY_300 = '#e5e5ea',
+    GRAY_400 = '#d1d1d6',
+    GRAY_500 = '#c7c7cc',
+    GRAY_600 = '#8e8e93',
+
+    /* Blue */
+
+    PRIMARY_BLUE = '#0d97d5',
+    LIGHT_PRIMARY_BLUE = '#f4fbfe',
+    SECONDARY_BLUE = '#007ce4',
+    THIRD_BLUE = '#54a4da',
+    LIGHT_THIRD_BLUE = '#e5f0f9',
+
+    /* Colors */
+
+    BLUE_500 = '#0D97D5',
+    INDIGO_500 = '#0D35D5',
+    YELLOW_500 = '#FFC72C',
+    ORANGE_500 = '#FF7649',
+    RED_500 = '#FF4949',
+    GREEN_500 = '#13CE64',
+    FUSCHIA_500 = '#AD0DD5',
+    PINK_500 = '#D50D99',
+    CYAN_500 = '#0DD5AD',
+
+
+    LIGHT_GREEN = '#e7fdf7',
+    LIGHT_YELLOW = '#fff0c9',
+    LIGHT_RED = '#ffdfdd',
+    VIOLET = '#8d51e0',
+    LIGHT_VIOLET = '#E4DEF5'
+}
