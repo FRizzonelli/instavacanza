56f8d63dd84bfe77560fc8c889d967d6d2c6acb8
frizzonelli <fabrizio.rizzonelli@gmail.com>
2019-08-09 16:30:55 +0200

    added configuration for navigation on Android

--- android/app/src/main/java/com/instavacanza/MainActivity.java
+++ android/app/src/main/java/com/instavacanza/MainActivity.java
@@ -1,15 +1,14 @@
 package com.instavacanza;
 
-import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
     @Override
-    protected String getMainComponentName() {
-        return "instavacanza";
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(this.createSplashLayout());
     }
 }
